c44131378ca80b22be7496582779b35fcc3a944c
jazmin <jazmincarlossc@gmail.com>
2025-05-20 09:36:46 +0200

    SUBIR

---
+++ Caronte/dockerfiles/ubuntu/admin/ssh/ssh.sh
@@ -0,0 +1,26 @@
+config_ssh(){
+    sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+    sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
+    if [ ! -d /home/${USUARIO}/.ssh ]
+    then
+        mkdir /home/${USUARIO}/.ssh
+        cat /root/datos/id_rsa.pub >> /home/${USUARIO}/.ssh/authorized_keys
+    fi
+    # /etc/init.d/ssh start &
+    exec /usr/sbin/sshd -D & # dejar el ssh en background (2plano)
+}
+#morgado ALL=(ALL:ALL) ALL
+config_sudoers(){
+    if [ -f /etc/sudoers ]
+    then 
+        #comprobar que el ${USUARIO} No existe
+        echo "${USUARIO} ALL=(ALL:ALL) ALL" >> /etc/sudoers
+    fi
+}
+
+newSSH(){
+    #gestion de errores y salida a logs
+    
+    config_sudoers
+    config_ssh
+}
---
+++ Caronte/dockerfiles/ubuntu/admin/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+source /root/admin/usuarios/usuarios.sh
+source /root/admin/ssh/ssh.sh
+
+chmod +x /root/admin/usuarios/usuarios.sh
+chmod +x /root/admin/ssh/ssh.sh
+
+main(){
+   newUser  
+#    if [ "$?" -eq 0 ]
+#    then 
+   newSSH
+   # make_bienvenida
+
+   # tail -f /dev/null (1plano)
+}
+
+main
